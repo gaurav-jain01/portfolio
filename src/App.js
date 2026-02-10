320c2afd25ab6253c5868731b23c79191134a635
Gaurav Jain <gaurav@hopewebsolution.com>
2026-02-10 15:27:42 +0530

    test 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Layoutpage from "./component/layout-page.js"
 import Test from "./component/test.js"
+import TestOne from "./component/testOne.js"
 
 function App() {
   return (
     <div className="App">
       <Layoutpage/>
       <Test/>
+      <TestOne/>
+      
     </div>
   );
 }
