bf1e20ea676bbbfd8c34175d89dcd1bd8a536193
Gaurav Jain <gaurav@hopewebsolution.com>
2026-02-10 15:22:56 +0530

    test one addded

--- src/component/testOne.js
+++ src/component/testOne.js
@@ -1,8 +1,8 @@
-function Test() {
+function TestOne() {
     return (
         <div>
             <h1>This is a test component</h1>
         </div>
     );
 }
-export default Test;
+export default TestOne;
