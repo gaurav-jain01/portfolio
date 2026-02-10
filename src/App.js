ac00de9c90871ffe240e9129926348e6fc5fac99
Gaurav Jain <gaurav@hopewebsolution.com>
2026-02-10 16:29:22 +0530

    testing complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import Layoutpage from "./component/layout-page.js"
-import Test from "./component/test.js"
 
 function App() {
   return (
     <div className="App">
       <Layoutpage/>
-      <Test/>
     </div>
   );
 }
